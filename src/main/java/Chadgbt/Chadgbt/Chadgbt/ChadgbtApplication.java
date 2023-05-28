edac9cd995107116a49fbbcc1f1ac62cf7111e19
Chen Kun <chenkun8991@gmail.com>
2023-05-28 16:04:20 +0800

    Setup

---
+++ src/main/java/Chadgbt/Chadgbt/Chadgbt/ChadgbtApplication.java
@@ -0,0 +1,13 @@
+package Chadgbt.Chadgbt.Chadgbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChadgbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChadgbtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Chadgbt/Chadgbt/Chadgbt/ChadgbtApplicationTests.java
@@ -0,0 +1,13 @@
+package Chadgbt.Chadgbt.Chadgbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChadgbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
